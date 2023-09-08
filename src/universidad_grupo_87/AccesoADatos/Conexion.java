ed0569e0adb998831c38ef1e73eb000f58d874b8
GabrielGelves <gabrieltombino84@gmail.com>
2023-09-08 19:34:22 -0300

    conexion

---
+++ src/universidad_grupo_87/AccesoADatos/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidad_grupo_87.AccesoADatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gabriel925
+ */
+public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="universidad";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion(){
+        
+    }
+    
+    public static Connection getConexion(){
+        if(connection == null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                JOptionPane.showMessageDialog(null,"Conexion Exitosa");
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"Error en la carga de Driver "+ ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error en la conexion con la base de datos "+ ex.getMessage());
+            }
+            
+        }
+        return connection;
+    }
+}
--- src/universidad_grupo_87/Universidad_Grupo_87.java
+++ src/universidad_grupo_87/Universidad_Grupo_87.java
@@ -5,6 +5,8 @@
  */
 package universidad_grupo_87;
 
+import universidad_grupo_87.AccesoADatos.Conexion;
+
 /**
  *
  * @author gabriel925
@@ -15,7 +17,9 @@ public class Universidad_Grupo_87 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Conexion.getConexion();
+        
     }
     
 }
